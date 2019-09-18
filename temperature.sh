e2b33f378c4c588a2e5227995987c7d380ba19cc
Bryce Zimmmerman <brycehzimmerman@gmail.com>
2019-09-18 16:16:52 -0500

    Added Kelvin conversion and improved formatting to temperature.sh

--- temperature.sh
+++ temperature.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 clear
-echo "Enter a temp in (F(deg) to be converted to C(deg).)"
+echo "Enter a temp in F(deg) to be converted to C(deg) and Kelvin."
 read tempf
+echo " "
 echo "scale=3;(($tempf-32)*(5/9))" | bc
+echo "Degrees Celsius"
+echo " "
+echo "scale=3;((($tempf-32)*(5/9))+273)" | bc
+echo "Kelvin"
